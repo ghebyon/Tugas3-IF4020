f9f4fdfa1230b24e65589568d5706a3af6bacd35
afrizalsebastian <sebastiangurning@gmail.com>
2023-04-18 10:32:28 +0700

    test in app.java

--- EncryptionDecryption/App.java
+++ EncryptionDecryption/App.java
@@ -20,135 +20,135 @@ public class App {
     }
 
     public static void main(String[] args) throws Exception {
-    //    KeyExpansion kExpansion = new KeyExpansion("H-2 Menuju UTS Semangat Gaes!", 16);
-    //    kExpansion.makeRoundKey();
-    //     // for(int i = 0; i < kExpansion.roundKey.size(); i++){
-    //     //         System.out.println(kExpansion.roundKey.get(i));
-    //     // }
+        KeyExpansion kExpansion = new KeyExpansion("H-2 Menuju UTS Semangat Gaes!", 16);
+        kExpansion.makeRoundKey();
+        // for(int i = 0; i < kExpansion.roundKey.size(); i++){
+        //         System.out.println(kExpansion.roundKey.get(i));
+        // }
 
-    //    Permutation permutation = new Permutation();
-    //     // for(int i = 0; i < kExpansion.roundKey.size(); i++){
-    //     //     System.out.println(kExpansion.roundKey.get(i).substring(2, kExpansion.roundKey.get(i).length()));
-    //     // }
-    //     List<Integer> p_box = permutation.generatePBox("H-2 Menuju UTS Semangat", 128);
-    //     // for(int i = 0; i < p_box.size(); i++){
-    //     //     System.out.print(p_box.get(i) + ",");
-    //     // }
-    //     byte[] byteArray = new byte[] { (byte)0x01, (byte)0x23, (byte)0x45, (byte)0x67, (byte)0x89, (byte)0xab, (byte)0xcd, (byte)0xef, (byte)0xfe, (byte)0xdc, (byte)0xba, (byte)0x98, (byte)0x76, (byte)0x54, (byte)0x32, (byte)0x10 };
+        Permutation permutation = new Permutation();
+        // for(int i = 0; i < kExpansion.roundKey.size(); i++){
+        //     System.out.println(kExpansion.roundKey.get(i).substring(2, kExpansion.roundKey.get(i).length()));
+        // }
+        List<Integer> p_box = permutation.generatePBox("H-2 Menuju UTS Semangat", 128);
+        // for(int i = 0; i < p_box.size(); i++){
+        //     System.out.print(p_box.get(i) + ",");
+        // }
+        byte[] byteArray = new byte[] { (byte)0x01, (byte)0x23, (byte)0x45, (byte)0x67, (byte)0x89, (byte)0xab, (byte)0xcd, (byte)0xef, (byte)0xfe, (byte)0xdc, (byte)0xba, (byte)0x98, (byte)0x76, (byte)0x54, (byte)0x32, (byte)0x10 };
         
-    //     byte[] x = permutation.permute(byteArray, p_box);
+        byte[] x = permutation.permute(byteArray, p_box);
 
-    //     byte[] y = permutation.netralize(x, p_box);
+        byte[] y = permutation.netralize(x, p_box);
         
 
-    //     Substitution substitution = new Substitution();
+        Substitution substitution = new Substitution();
 
-    //     // System.out.println("SUBSTITUTION");
-    //     List<Integer> s_box = substitution.generateSBox("H-2 Menuju UTS Semangat Gaes!", 256);
+        // System.out.println("SUBSTITUTION");
+        List<Integer> s_box = substitution.generateSBox("H-2 Menuju UTS Semangat Gaes!", 256);
 
-    //     List<Integer> i_s_box = substitution.inverseSBox(s_box);
+        List<Integer> i_s_box = substitution.inverseSBox(s_box);
 
-    //     byte[] subs = substitution.substitute(byteArray, s_box);
+        byte[] subs = substitution.substitute(byteArray, s_box);
 
-    //     byte[] reverse = substitution.reverse(
-    //         subs, s_box);
-    //     // for (int i=0; i < reverse.length; i++){
-    //     //     System.out.print(reverse[i]+ ", ");
-    //     // }
+        byte[] reverse = substitution.reverse(
+            subs, s_box);
+        // for (int i=0; i < reverse.length; i++){
+        //     System.out.print(reverse[i]+ ", ");
+        // }
 
-    //     RoundFunction roundFunction = new RoundFunction("halokamusiapahah".getBytes(), kExpansion.roundKey, 16, true);
-    //     roundFunction.encrypt();
-    //     System.out.println(new String(roundFunction.ciphertext));
-    //     RoundFunction roundFunction1 = new RoundFunction(roundFunction.ciphertext, kExpansion.roundKey, 16, false);
-    //     roundFunction1.decrypt();
-    //     System.out.println(new String(roundFunction.plaintext, StandardCharsets.UTF_8));
+        RoundFunction roundFunction = new RoundFunction("halokamusiapahah".getBytes(), kExpansion.roundKey, 16, true);
+        roundFunction.encrypt();
+        System.out.println(new String(roundFunction.ciphertext));
+        RoundFunction roundFunction1 = new RoundFunction(roundFunction.ciphertext, kExpansion.roundKey, 16, false);
+        roundFunction1.decrypt();
+        System.out.println(new String(roundFunction.plaintext, StandardCharsets.UTF_8));
 
-        byte[] cipher = new byte[0];
-        while (true){
-            System.out.println("Go-Block-Cipher");
-            System.out.println("===============");
-            System.out.println("1. Encrypt");
-            System.out.println("2. Decrypt");
-            System.out.println("3. Exit");
-            System.out.println("===============");
-            System.out.print("Choice : ");
-            Scanner sc= new Scanner(System.in);
-            int choice = sc.nextInt();
-            System.out.println("===============");
+        // byte[] cipher = new byte[0];
+        // while (true){
+        //     System.out.println("Go-Block-Cipher");
+        //     System.out.println("===============");
+        //     System.out.println("1. Encrypt");
+        //     System.out.println("2. Decrypt");
+        //     System.out.println("3. Exit");
+        //     System.out.println("===============");
+        //     System.out.print("Choice : ");
+        //     Scanner sc= new Scanner(System.in);
+        //     int choice = sc.nextInt();
+        //     System.out.println("===============");
 
-            if(choice == 1){
-                System.out.print("Key : ");
-                String key = sc.next();
-                System.out.print("Plaintext : ");
-                String plaintext = sc.next();
-                byte[] bytePlainText = plaintext.getBytes();
+        //     if(choice == 1){
+        //         System.out.print("Key : ");
+        //         String key = sc.next();
+        //         System.out.print("Plaintext : ");
+        //         String plaintext = sc.next();
+        //         byte[] bytePlainText = plaintext.getBytes();
 
-                //Key Expansion
-                KeyExpansion keyExpansion = new KeyExpansion(key, 16);
-                keyExpansion.makeRoundKey();
+        //         //Key Expansion
+        //         KeyExpansion keyExpansion = new KeyExpansion(key, 16);
+        //         keyExpansion.makeRoundKey();
 
-                byte[][] plaintextChunks = splitText(bytePlainText);
-                byte[] ciphertext = new byte[0];
+        //         byte[][] plaintextChunks = splitText(bytePlainText);
+        //         byte[] ciphertext = new byte[0];
 
-                Permutation permutation = new Permutation();
-                Substitution substitution = new Substitution();
+        //         Permutation permutation = new Permutation();
+        //         Substitution substitution = new Substitution();
 
-                List<Integer> pBox = permutation.generatePBox(key, 128);
-                List<Integer> sBox = substitution.generateSBox(key, 256);
+        //         List<Integer> pBox = permutation.generatePBox(key, 128);
+        //         List<Integer> sBox = substitution.generateSBox(key, 256);
                 
-                int i = 0;
-                for (byte[] pln : plaintextChunks){
-                    byte[] chunk = permutation.permute(pln, pBox);
+        //         int i = 0;
+        //         for (byte[] pln : plaintextChunks){
+        //             byte[] chunk = permutation.permute(pln, pBox);
 
-                    RoundFunction roundFunction = new RoundFunction(chunk, keyExpansion.roundKey, 16, true);
-                    roundFunction.encrypt();
+        //             RoundFunction roundFunction = new RoundFunction(chunk, keyExpansion.roundKey, 16, true);
+        //             roundFunction.encrypt();
 
-                    byte[] ciphertext_chunks = roundFunction.ciphertext;
+        //             byte[] ciphertext_chunks = roundFunction.ciphertext;
 
-                    ciphertext_chunks = substitution.substitute(ciphertext_chunks, sBox);
+        //             ciphertext_chunks = substitution.substitute(ciphertext_chunks, sBox);
 
-                    ciphertext = roundFunction.concat(ciphertext, ciphertext_chunks);
+        //             ciphertext = roundFunction.concat(ciphertext, ciphertext_chunks);
 
-                    i++;
-                }
-                cipher = ciphertext;
-                System.out.println( "Ciphetext : " + new String(ciphertext, StandardCharsets.UTF_8));
-            }
-            else if(choice == 2){
-                System.out.print("Key : ");
-                String key = sc.next();
+        //             i++;
+        //         }
+        //         cipher = ciphertext;
+        //         System.out.println( "Ciphetext : " + new String(ciphertext, StandardCharsets.UTF_8));
+        //     }
+        //     else if(choice == 2){
+        //         System.out.print("Key : ");
+        //         String key = sc.next();
 
-                KeyExpansion keyExpansion = new KeyExpansion(key, 16);
-                keyExpansion.makeRoundKey();
+        //         KeyExpansion keyExpansion = new KeyExpansion(key, 16);
+        //         keyExpansion.makeRoundKey();
 
-                byte[][] ciphertext_chunks = splitText(cipher);
-                byte[] plaintext = new byte[0];
+        //         byte[][] ciphertext_chunks = splitText(cipher);
+        //         byte[] plaintext = new byte[0];
 
-                Permutation permutation = new Permutation();
-                Substitution substitution = new Substitution();
+        //         Permutation permutation = new Permutation();
+        //         Substitution substitution = new Substitution();
 
-                List<Integer> pBox = permutation.generatePBox(key, 128);
-                List<Integer> sBox = substitution.generateSBox(key, 256);
+        //         List<Integer> pBox = permutation.generatePBox(key, 128);
+        //         List<Integer> sBox = substitution.generateSBox(key, 256);
 
-                int i = 0;
-                for(byte[] cpr : ciphertext_chunks){
-                    byte[] chunk = substitution.reverse(cpr, sBox);
+        //         int i = 0;
+        //         for(byte[] cpr : ciphertext_chunks){
+        //             byte[] chunk = substitution.reverse(cpr, sBox);
 
-                    RoundFunction roundFunction = new RoundFunction(chunk, keyExpansion.roundKey, 16, false);
-                    roundFunction.decrypt();
+        //             RoundFunction roundFunction = new RoundFunction(chunk, keyExpansion.roundKey, 16, false);
+        //             roundFunction.decrypt();
 
-                    byte[] plaintextChunks = roundFunction.plaintext;
+        //             byte[] plaintextChunks = roundFunction.plaintext;
 
-                    plaintextChunks = permutation.netralize(plaintextChunks, pBox);
+        //             plaintextChunks = permutation.netralize(plaintextChunks, pBox);
 
-                    plaintext = roundFunction.concat(plaintext, plaintextChunks);
+        //             plaintext = roundFunction.concat(plaintext, plaintextChunks);
 
-                    i++;
-                }
+        //             i++;
+        //         }
 
-                System.out.println("Plaintext : " + new String(plaintext, StandardCharsets.UTF_8));
-            }
-        }
+        //         System.out.println("Plaintext : " + new String(plaintext, StandardCharsets.UTF_8));
+        //     }
+        // }
 
     }
 }
